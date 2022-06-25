5b450046c84c7100a3a8aa7a1ca65d2cc0f4a780
ODAdebayo <58173405+ODAdebayo@users.noreply.github.com>
2022-06-25 15:17:47 +0100

    changes to app transactions

--- src/database/migrations/2022_06_23_140434_create_web_transactions_table.php
+++ src/database/migrations/2022_06_23_140434_create_web_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentsTable extends Migration
+class CreateWebTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePaymentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('payments', function (Blueprint $table) {
-            $table->id();
+        Schema::create('web_transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('order_code');
-            $table->decimal('total', 10,2);
+            $table->decimal('total', 8,2);
             $table->integer('status');
             $table->timestamps();
         });
@@ -29,6 +29,6 @@ class CreatePaymentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('payments');
+        Schema::dropIfExists('web_transactions');
     }
 }
